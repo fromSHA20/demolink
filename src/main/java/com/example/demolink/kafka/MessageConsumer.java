856c459af12f1d098ee806c989e63bbdad3c1f1b
fromSHA20 <vak.90@inbox.ru>
2026-08-23 14:53:15 +0300

    Обновлено 23_1/08/26

--- src/main/java/com/example/demolink/kafka/MessageConsumer.java
+++ src/main/java/com/example/demolink/kafka/MessageConsumer.java
@@ -16,7 +16,7 @@ public class MessageConsumer {
     public void receiveEmail(String message) {
         System.out.println("get message: email-model");
         System.out.println(message);
-        System.out.println("123");
+        System.out.println("12");
     }
 
 
