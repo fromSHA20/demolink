b9c0c932579f7384859b8cc244d28f85e2c28e85
fromSHA20 <vak.90@inbox.ru>
2026-08-23 12:29:17 +0300

    Обновлено

---
+++ email/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+
+public class DemolinkApplication {
+
+    @SpringBootApplication
+    public static void main(String[] args) {
+        SpringApplication.run(DemolinkApplication.class, args);
+
+
+    }
+
+}
