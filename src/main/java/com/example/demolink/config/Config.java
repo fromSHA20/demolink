9bba8a5af4ad581d74573a800a5b07c014ff6a34
fromSHA20 <vak.90@inbox.ru>
2026-08-23 15:17:17 +0300

    Обновлено 23/08/26

--- src/main/java/com/example/demolink/config/Config.java
+++ src/main/java/com/example/demolink/config/Config.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 public class Config {
     @Bean("getText")
     public String getText() {
+        System.out.println("Hello world");
         return "Hello world, kak dela? ";
     }
 
